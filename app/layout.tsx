49a114d737582c06a9a894af8c2f0e2ca484fa90
ArminRezaei <arminrezaei53@gmail.com>
2024-02-11 14:03:08 +0330

    implement theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 import { Inter, Space_Grotesk } from 'next/font/google'
 import { Metadata } from 'next'
+import { ThemeProvider } from '@/context/ThemeProvider'
 export const metaData: Metadata = {
   title: "DevFlow",
   description: 'A community driven platform for developers asking and answering questions.Get help and answers to your questions about anything related to programming and software development from the best developers on the internet.',
@@ -25,19 +26,24 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider
-    appearance={{
-      elements:{
-        formButtonPrimary:'primary-gradient',
-        footerActionLink:'primary-text-gradient hover:text-primary-500'
-      }
-    }}
-    >
-      <html lang="en" className={`${inter.variable}${spaceGrotest.variable}`}>
 
-        <body>
-          {children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang="en" className={`${inter.variable}${spaceGrotest.variable}`}>
+
+      <body>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: 'primary-gradient',
+              footerActionLink: 'primary-text-gradient hover:text-primary-500'
+            }
+          }}
+        >
+          <ThemeProvider>
+            {children}
+          </ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+
   )
 }
